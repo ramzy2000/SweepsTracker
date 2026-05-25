b1038a487bc0caf89f8cdbb18882aed4d80f53e2
Casey Duffy <caseyduffy00@gmail.com>
2026-05-25 14:35:52 -0500

    Fet/sweeps tracker 1/home menu (#2)
    * sweeps-tracker-1: add maui blazor boiler plate
    
    * sweeps-tracker-1: add home menu buttons and logo image
    
    * sweeps-tracker-1: add AppState Singleton class to keep track of app state

---
+++ AppState.cs
@@ -0,0 +1,4 @@
+public class AppState
+{
+    public bool isBackButton = false;
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace SweepsTracker;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
