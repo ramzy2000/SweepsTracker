fe749f373921bdc370ab45bafe3bd3d061a6148c
cduffy-tri <caseyduffy00@gmail.com>
2026-05-25 14:28:36 -0500

    sweeps-tracker-1: add AppState Singleton class to keep track of app state

--- MauiProgram.cs
+++ MauiProgram.cs
@@ -7,6 +7,7 @@ public static class MauiProgram
 	public static MauiApp CreateMauiApp()
 	{
 		var builder = MauiApp.CreateBuilder();
+		builder.Services.AddSingleton<AppState>();
 		builder
 			.UseMauiApp<App>()
 			.ConfigureFonts(fonts =>
