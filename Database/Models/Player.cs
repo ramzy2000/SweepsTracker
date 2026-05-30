9fe784f03785f28fcad75ce78610daea1b30f819
cduffy-tri <caseyduffy00@gmail.com>
2026-05-29 19:53:37 -0500

    sweeps-tracker-6: add player database model

---
+++ Database/Models/Player.cs
@@ -0,0 +1,14 @@
+using System.ComponentModel.DataAnnotations.Schema;
+using SQLite;
+
+public class Player
+{
+    [PrimaryKey, AutoIncrement]
+    public int ID { get; set; }
+
+    public int GameID { get; set; }
+
+    public string Name { get; set; }
+
+    public int TotalScore { get; set; }
+}
