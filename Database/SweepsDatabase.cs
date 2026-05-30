45e9e0a2d0661e462bf26f022e1208da902258b8
cduffy-tri <caseyduffy00@gmail.com>
2026-05-29 20:19:39 -0500

    sweeps-tracker-6: add CreateNewGame function to the database access class

--- Database/SweepsDatabase.cs
+++ Database/SweepsDatabase.cs
@@ -14,4 +14,25 @@ public class SweepsDatabase
         database.CreateTableAsync<Round>().Wait();
         database.CreateTableAsync<RoundScore>().Wait();
     }
+
+    public async Task<bool> CreateNewGame(Game game, List<Player> players)
+    {
+        // create the game record
+        int createGame = await database.InsertAsync(game);
+
+        // set player GameID to the created game ID
+        for(int i = 0; i < players.Count(); i++)
+        {
+            players[i].GameID = game.ID;
+        }
+        
+        // create player records
+        int createdPlayer = await database.InsertAllAsync(players);
+
+        if(createGame > 0 && createdPlayer > 0)
+        {
+            return true;
+        }
+        return false;
+    }
 }
